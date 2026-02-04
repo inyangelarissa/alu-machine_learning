527386e0b30aafc54edc9dc2d0fe99c2f930e126
inyangelarissa <l.inyange@alustudent.com>
2026-02-04 17:54:32 +0200

    a few change

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+def matrix_shape(matrix):
+    """
+    Calculates the shape of a matrix
+
+    Args:
+        matrix: A matrix (nested list)
+
+    Returns:
+        A list of integers representing the shape
+    """
+    shape = []
+    current = matrix
+
+    while isinstance(current, list):
+        shape.append(len(current))
+        current = current[0]
+
+    return shape
