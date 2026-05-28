d1f95511a70786b5f030172ed969ac4936a923a6
inyangelarissa <l.inyange@alustudent.com>
2026-05-28 12:04:02 +0200

    Update 3-calculate_accuracy.py

--- supervised_learning/tensorflow/3-calculate_accuracy.py
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python3
-""" train"""
+""" create layer """
 import tensorflow as tf
 
 
-def evaluate(X, Y, save_path):
-    """ evaluate"""
-    with tf.Session() as sess:
-        saver = tf.train.import_meta_graph(save_path + '.meta')
-        saver.restore(sess, save_path)
-        x = tf.get_collection('x')[0]
-        y = tf.get_collection('y')[0]
-        y_pred = tf.get_collection('y_pred')[0]
-        accuracy = tf.get_collection('accuracy')[0]
-        loss = tf.get_collection('loss')[0]
-        pred = sess.run(y_pred, feed_dict={x: X, y: Y})
-        acc = sess.run(accuracy, feed_dict={x: X, y: Y})
-        ls = sess.run(loss, feed_dict={x: X, y: Y})
-    return pred, acc, ls
+def calculate_accuracy(y, y_pred):
+    """ calculate accuracy"""
+    p_m = tf.arg_max(y_pred, 1)
+    y_m = tf.arg_max(y, 1)
+    e = tf.equal(y_m, p_m)
+    return tf.reduce_mean(tf.cast(e, tf.float32))
