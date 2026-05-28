180b203984179b103bc72a938f7c9cf04360bf79
inyangelarissa <l.inyange@alustudent.com>
2026-05-28 12:01:00 +0200

    Update 3-calculate_accuracy.py

--- supervised_learning/tensorflow/3-calculate_accuracy.py
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
-""" create layer """
-
-
+""" train"""
 import tensorflow as tf
-create_layer = __import__('1-create_layer').create_layer
 
 
-def forward_prop(x, layer_sizes=[], activations=[]):
-    """ forward prop"""
-    L = x
-    for i in range(len(layer_sizes)):
-        L = create_layer(L, layer_sizes[i],
-                         activations[i])
-    return L
+def evaluate(X, Y, save_path):
+    """ evaluate"""
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph(save_path + '.meta')
+        saver.restore(sess, save_path)
+        x = tf.get_collection('x')[0]
+        y = tf.get_collection('y')[0]
+        y_pred = tf.get_collection('y_pred')[0]
+        accuracy = tf.get_collection('accuracy')[0]
+        loss = tf.get_collection('loss')[0]
+        pred = sess.run(y_pred, feed_dict={x: X, y: Y})
+        acc = sess.run(accuracy, feed_dict={x: X, y: Y})
+        ls = sess.run(loss, feed_dict={x: X, y: Y})
+    return pred, acc, ls
