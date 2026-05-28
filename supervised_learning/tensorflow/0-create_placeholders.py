51f5596b17fb1a02773774cee6f21b6aeef80c6d
inyangelarissa <l.inyange@alustudent.com>
2026-05-28 12:06:09 +0200

    Fix formatting by adding newline at end of file

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -16,3 +16,4 @@ def create_placeholders(nx, classes):
     x = tf.placeholder("float", shape=[None, nx], name="x")
     y = tf.placeholder("float", shape=[None, classes], name="y")
     return x, y
+    
