9e1292222cc90e321c6031eb5538b9355fd42d02
inyangelarissa <l.inyange@alustudent.com>
2026-06-02 22:15:40 +0200

    Refactor Bayesian optimization code structure

--- unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
@@ -1,34 +1,46 @@
-from scipy.stats import norm
+#!/usr/bin/env python3
+"""Bayesian Optimization"""
+
 import numpy as np
+from scipy.stats import norm
 
-def acquisition(self):
-    """
-    Calculates the next best sample location using
-    the Expected Improvement acquisition function.
+GP = __import__('2-gp').GaussianProcess
 
-    Returns:
-        X_next: numpy.ndarray of shape (1,)
-        EI: numpy.ndarray of shape (ac_samples,)
-    """
 
-    mu, sigma = self.gp.predict(self.X_s)
+class BayesianOptimization:
+    """Performs Bayesian optimization on a noiseless 1D Gaussian process"""
 
-    sigma = sigma.reshape(-1)
+    def __init__(self, f, X_init, Y_init, bounds,
+                 ac_samples, l=1, sigma_f=1,
+                 xsi=0.01, minimize=True):
+        self.f = f
+        self.gp = GP(X_init, Y_init, l=l, sigma_f=sigma_f)
+        self.X_s = np.linspace(
+            bounds[0],
+            bounds[1],
+            ac_samples
+        ).reshape(-1, 1)
+        self.xsi = xsi
+        self.minimize = minimize
 
-    if self.minimize:
-        Y_best = np.min(self.gp.Y)
-        improvement = Y_best - mu - self.xsi
-    else:
-        Y_best = np.max(self.gp.Y)
-        improvement = mu - Y_best - self.xsi
+    def acquisition(self):
+        """Calculates the next best sample location"""
 
-    with np.errstate(divide='ignore'):
-        Z = improvement / sigma
+        mu, sigma = self.gp.predict(self.X_s)
+        sigma = sigma.reshape(-1)
 
-        EI = improvement * norm.cdf(Z) + sigma * norm.pdf(Z)
+        if self.minimize:
+            Y_best = np.min(self.gp.Y)
+            improvement = Y_best - mu - self.xsi
+        else:
+            Y_best = np.max(self.gp.Y)
+            improvement = mu - Y_best - self.xsi
 
-        EI[sigma == 0.0] = 0.0
+        with np.errstate(divide='ignore'):
+            Z = improvement / sigma
+            EI = improvement * norm.cdf(Z) + sigma * norm.pdf(Z)
+            EI[sigma == 0] = 0
 
-    X_next = self.X_s[np.argmax(EI)]
+        X_next = self.X_s[np.argmax(EI)]
 
-    return X_next, EI
+        return X_next, EI
