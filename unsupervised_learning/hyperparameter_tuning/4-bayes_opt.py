8f2beb730590e53b039444c2d90bbb3bf3a84829
inyangelarissa <l.inyange@alustudent.com>
2026-06-02 22:18:32 +0200

    Update 4-bayes_opt.py

--- unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
@@ -8,18 +8,22 @@ GP = __import__('2-gp').GaussianProcess
 
 
 class BayesianOptimization:
-    """Performs Bayesian optimization on a noiseless 1D Gaussian process"""
+    """Performs Bayesian optimization on a noiseless 1D GP"""
 
     def __init__(self, f, X_init, Y_init, bounds,
                  ac_samples, l=1, sigma_f=1,
                  xsi=0.01, minimize=True):
+        """Class constructor"""
+
         self.f = f
         self.gp = GP(X_init, Y_init, l=l, sigma_f=sigma_f)
+
         self.X_s = np.linspace(
             bounds[0],
             bounds[1],
             ac_samples
         ).reshape(-1, 1)
+
         self.xsi = xsi
         self.minimize = minimize
 
@@ -27,19 +31,18 @@ class BayesianOptimization:
         """Calculates the next best sample location"""
 
         mu, sigma = self.gp.predict(self.X_s)
-        sigma = sigma.reshape(-1)
 
         if self.minimize:
-            Y_best = np.min(self.gp.Y)
-            improvement = Y_best - mu - self.xsi
+            optimum = np.min(self.gp.Y)
+            imp = optimum - mu - self.xsi
         else:
-            Y_best = np.max(self.gp.Y)
-            improvement = mu - Y_best - self.xsi
+            optimum = np.max(self.gp.Y)
+            imp = mu - optimum - self.xsi
 
         with np.errstate(divide='ignore'):
-            Z = improvement / sigma
-            EI = improvement * norm.cdf(Z) + sigma * norm.pdf(Z)
-            EI[sigma == 0] = 0
+            Z = imp / sigma
+            EI = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
+            EI[sigma == 0.0] = 0.0
 
         X_next = self.X_s[np.argmax(EI)]
 
